1a88dcdf92783ca5390116fa81f05c1dc87ae90a
William L'Eriger <espermentalout@gmail.com>
2019-02-05 15:21:32 -0500

    P1: added reset once you reach the top, making it an endless loop where the new well gets larger

--- projects/project1/project1/js/script.js
+++ projects/project1/project1/js/script.js
@@ -15,28 +15,41 @@ $(document).ready(setup);
 // $addBrick will be triggerred once the player presses a button
 // $climbOut activates once they reach the top of the well
 let $addBrick;
-let $clickCanvas;
+let $canvas;
 let $bricksStacked;
+let $well;
 
 // function setup()
 //
 // Here I define things that will load only once at the start
 function setup() {
 
-  $clickCanvas = $('#canvas');
+  $canvas = $('#canvas');
+  $well = $('#wellBackground');
   $addBrick = $("#bricks");
   $bricksStacked = 0;
 
-  $clickCanvas.on ("click", function(){
-    $addBrick.append("<div>BRICK</div>");
-    $bricksStacked++;
-    console.log($bricksStacked);
+  $canvas.on ("click", function(){
+
+    if ($bricksStacked < 12) {
+      $addBrick.append("<div>BRICK</div>");
+      $bricksStacked++;
+      console.log($bricksStacked);
+    } else if ($bricksStacked >= 12) {
+      $addBrick.empty();
+      $bricksStacked = 0;
+      let width = parseInt($well.css('width'));
+      width+= 50;
+      $well.css('width', width +'px')
+    }
   });
 }
 
 // function newWell()
 //
 // this function will enlarge the previous well and erase all the bricks gathered, resetting player at the // bottom
-function newWell() {
-
-}
+// function newWell() {
+//   if ($bricksStacked === 12) {
+//     $addBrick.remove("<div>BRICK</div>");
+//   }
+// }
