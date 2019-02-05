67a936992853ec254483cff717ed121545719faa
William L'Eriger <espermentalout@gmail.com>
2019-02-05 14:47:00 -0500

    P1: Fixed index divs, added functionality to add bricks by clicking, using the .append

--- projects/project1/project1/js/script.js
+++ projects/project1/project1/js/script.js
@@ -15,19 +15,25 @@ $(document).ready(setup);
 // $addBrick will be triggerred once the player presses a button
 // $climbOut activates once they reach the top of the well
 let $addBrick;
-let $climbOut;
+let $clickCanvas;
 
 // function setup()
 //
 // Here I define things that will load only once at the start
 function setup() {
 
+  $clickCanvas = $('#canvas');
+  $addBrick = $("#bricks");
 
+  $clickCanvas.on ("click", function(){
+    console.log('yes');
+    $addBrick.append("<div>BRICK</div>");
+  });
 }
 
 // function newWell()
 //
 // this function will enlarge the previous well and erase all the bricks gathered, resetting player at the // bottom
 function newWell() {
-
+  
 }
