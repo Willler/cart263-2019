3919f1da9090a527e20ea185ba3407ba4b42aea0
William L'Eriger <espermentalout@gmail.com>
2019-02-05 17:59:32 -0500

    P1: Added sounds

--- projects/project1/project1/js/script.js
+++ projects/project1/project1/js/script.js
@@ -19,6 +19,9 @@ let $canvas;
 let $bricksStacked;
 let $well;
 
+let bgMusic = new Audio("assets/sounds/lullaby.mp3");
+let brickSFX = new Audio("assets/sounds/brick.wav");
+
 // function setup()
 //
 // Here I define things that will load only once at the start
@@ -46,6 +49,9 @@ function setup() {
       $bricksStacked++;
     // check if $bricksStacked works as intended
       console.log($bricksStacked);
+    // play the brick sound effect
+      brickSFX.currentTime = 0;
+      brickSFX.play();
 
   // else if to check if bricks are equal to or greater than 12, which is the height of the well
     } else if ($bricksStacked >= 12) {
@@ -56,7 +62,7 @@ function setup() {
       $bricksStacked = 0;
     // adjust the width of the well by adding to it, calling the css values for the wellBackground div
       let width = parseInt($well.css('width'));
-      width+= 25;
+      width+= 8;
       $well.css('width', width +'px')
 
       $('.hiddenMockery').removeClass('hiddenMockery').addClass('mockery');
@@ -68,4 +74,7 @@ function setup() {
   $( "#prompt" ).dialog({
   position: {at: "left center" }
 });
+
+  bgMusic.play();
+  bgMusic.loop = true;
 }
