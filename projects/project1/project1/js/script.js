bdb0d5607980b29dc8d792f83d6dc0bb02e27f00
William L'Eriger <espermentalout@gmail.com>
2019-02-05 13:50:28 -0500

    P1: Added some divs for displayed items like the player letter and bricks, then styled it using flexbox in css, also layed down some initial variables and functions in the script

--- projects/project1/project1/js/script.js
+++ projects/project1/project1/js/script.js
@@ -3,7 +3,31 @@
 Well Over your Head
 William L'Eriger
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Try to escape a well, only to find out that at the top, there is another well!
 
 ******************/
+
+//document ready defined to the setup function
+//
+$(document).ready(setup);
+
+// defining some variables I will inevitably use
+// $addBrick will be triggerred once the player presses a button
+// $climbOut activates once they reach the top of the well
+let $addBrick;
+let $climbOut;
+
+// function setup()
+//
+// Here I define things that will load only once at the start
+function setup() {
+
+
+}
+
+// function newWell()
+//
+// this function will enlarge the previous well and erase all the bricks gathered, resetting player at the // bottom
+function newWell() {
+
+}
