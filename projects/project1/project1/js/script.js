1ec0fda1b44cc878ea4079d1b05ad3560434bb20
William L'Eriger <espermentalout@gmail.com>
2019-02-05 13:30:28 -0500

    P1: Started Project 1 with some basic page layout and css

---
+++ projects/project1/project1/js/script.js
@@ -0,0 +1,9 @@
+/*****************
+
+Well Over your Head
+William L'Eriger
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
