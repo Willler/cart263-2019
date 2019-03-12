2572c6a59c83f90a5b82869b0b93779c7be010af
William L'Eriger <espermentalout@gmail.com>
2019-03-12 10:19:45 -0400

    P2: More initializing, addind json arrays for input choices

--- projects/project2/project2/js/script.js
+++ projects/project2/project2/js/script.js
@@ -2,10 +2,31 @@
 
 /*****************
 
-Title of Project
-Author Name
+Caricature
+William L'Eriger
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+A project which allows the player to reconstruct an experience of growing up using the internet
+on a small scale
 
 ******************/
+
+$(document).ready(setup);
+
+// setup
+//
+// initializing variables
+function setup() {
+
+  // Start by displaying a text pop-up to introduce player to the game
+  $( "#startGameText" ).dialog({
+    //position the dialog window in the center of the canvas
+      position: {at: "center" }
+  });
+
+  // loading in json files
+  $.getJSON('assets/data/art.json', dataLoaded);
+  $.getJSON('assets/data/moods.json', dataLoaded);
+  $.getJSON('assets/data/animals.json', dataLoaded);
+  $.getJSON('assets/data/flowers.json', dataLoaded);
+  $.getJSON('assets/data/countries.json', dataLoaded);
+}
