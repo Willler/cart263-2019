f3995a8826f92f370d2575b7e2238b613ed18c63
William L'Eriger <espermentalout@gmail.com>
2019-03-19 11:02:03 -0400

    P3: Started Project 3

---
+++ projects/project3/project3/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*****************
+
+Project 3 [Name Pending]
+William L'Eriger
+
+Javascript code for Project 3
+A series of minigame pop-ups depicting daydreams
+In a broader theme of Escapism
+
+******************/
