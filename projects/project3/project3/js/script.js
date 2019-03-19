e93f72f83e6389bb434d399959e380a5b16ed2f2
William L'Eriger <espermentalout@gmail.com>
2019-03-19 12:28:55 -0400

    P3: Started the basic framework for jqueryUI dialog widgets, which will house my minigames

--- projects/project3/project3/js/script.js
+++ projects/project3/project3/js/script.js
@@ -10,3 +10,44 @@ A series of minigame pop-ups depicting daydreams
 In a broader theme of Escapism
 
 ******************/
+
+$(document).ready(setup);
+
+function setup() {
+  $(this).one('click', minigame1);
+}
+
+function minigame1() {
+  $('body').append("<div class = 'mini1'><div>");
+  console.log("mini1");
+
+  $(".mini1").text("Minigame 1")
+  $(".mini1").dialog({
+  //position the dialog window in the center of the canvas
+  position: {
+    at: "center"
+  },
+  height: 380,
+  width: 550,
+  close: minigame2()
+});
+
+  // $(".mini").on("dialogclose", minigame2);
+}
+
+function minigame2() {
+  $(".mini1").remove();
+  $('body').append("<div class = 'mini2'><div>");
+  console.log("mini2");
+
+  $(".mini2").text("Minigame 2")
+  $(".mini2").dialog({
+  //position the dialog window in the center of the canvas
+  position: {
+    at: "center"
+  },
+  height: 380,
+  width: 550,
+  // close: function(minigame2, ui);
+});
+}
