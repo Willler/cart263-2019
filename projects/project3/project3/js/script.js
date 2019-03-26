aab409c0bbdf87c656868e6d2d87c222d87593cb
William L'Eriger <espermentalout@gmail.com>
2019-03-26 10:58:01 -0400

    P3: Added a third minigame and the ability to choose between which one minigame to proceed to in the first one

--- projects/project3/project3/js/script.js
+++ projects/project3/project3/js/script.js
@@ -19,9 +19,9 @@ function setup() {
 
 function minigame1() {
   $('body').append("<div class = 'mini1'><div>");
-  console.log("mini1");
+  console.log("First Minigame - Initiated");
 
-  $(".mini1").text("Minigame 1")
+  $(".mini1").text("The First Minigame, which basically chooses the path to take, using annyang.")
   $(".mini1").dialog({
   //position the dialog window in the center of the canvas
   position: {
@@ -29,12 +29,27 @@ function minigame1() {
   },
   height: 380,
   width: 550,
-  close: minigame2() 
+  // close: minigame2,
+  closeOnEscape: false,
+  title: "The First Layer",
+  buttons: [{
+
+    text: "2",
+    icon: "ui-icon-gear",
+    click: minigame2,
+  },{
+    text: "3",
+    icon: "ui-icon-heart",
+    click: minigame3
+  }
+]
 });
+
 }
 
 function minigame2() {
   $(".mini1").remove();
+  console.log("Second Minigame - Initiated");
   $('body').append("<div class = 'mini2'><div>");
 
   $(".mini2").text("Minigame 2")
@@ -46,5 +61,27 @@ function minigame2() {
   height: 380,
   width: 550,
   // close: function(minigame2, ui);
+  closeOnEscape: false,
+  title: "The Second Layer"
 });
+}
+
+function minigame3() {
+    $(".mini1").remove();
+  $('body').append("<div class = 'mini3'><div>");
+  console.log("Third Minigame - Initiated");
+
+  $(".mini3").text("The Third Minigame.")
+  $(".mini3").dialog({
+  //position the dialog window in the center of the canvas
+  position: {
+    at: "center"
+  },
+  height: 380,
+  width: 550,
+  // close: minigame2,
+  closeOnEscape: false,
+  title: "The Third Layer"
+});
+
 }
